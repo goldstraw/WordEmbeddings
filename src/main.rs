f349767e217ea961b418346a09f1c5c118dfbe19
Charlie Goldstraw <goldstrawcharlie@gmail.com>
2023-05-03 16:45:59 +0100

    Implement co-occurrence matrix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 struct Review {
     review: String,
     sentiment: bool,
@@ -52,6 +54,43 @@ fn load_imdb_dataset(path: &str) -> Vec<Review> {
     imdb_dataset
 }
 
+fn build_vocab(dataset: &Vec<Review>) -> Vec<String> {
+    let mut vocab: Vec<String> = Vec::new();
+    for review in dataset {
+        for word in review.review.split_whitespace() {
+            if !vocab.contains(&word.to_string()) {
+                vocab.push(word.to_string());
+            }
+        }
+    }
+    vocab
+}
+
+fn build_co_occurrence_matrix() {
+
+}
+
 fn main() {
     let imdb_dataset = load_imdb_dataset("imdb_dataset.csv");
+    let vocab = build_vocab(&imdb_dataset);
+    let co_occurrence_window = 3;
+    let co_occurrence_matrix = HashMap::new();
+    for review in imdb_dataset {
+        let mut words = review.review.split_whitespace();
+        let mut index = 0;
+        for word in words {
+            let mut index2 = 0;
+            for word2 in words {
+                let dist = (index - index2).abs();
+                if dist <= co_occurrence_window {
+                    let count = co_occurrence_matrix.entry(word).or_insert(HashMap::new());
+                    let weight = 1.0 - (dist as f32 / co_occurrence_window as f32);
+                    let count2 = count.entry(word2).or_insert(0.0);
+                    *count2 += weight;
+                }
+                index2 += 1;
+            }
+            index += 1;
+        }
+    }
 }
